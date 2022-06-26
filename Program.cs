ba8530af6019e9ad3c26cfacc4ae3bd4ed67fd21
Michael <99751943+Kutsarskiy@users.noreply.github.com>
2022-06-26 16:40:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int mode = 0;
+            while (mode != 1)
+            {
+                Console.Write("Чтобы выбрать задание нажмите [1], [2] или [3]. Для выхода из программы нажмите [4]: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        mode = 0;
+                        Task1.First();
+                        break;
+
+                    case 2:
+                        mode = 0;
+                        Task2.Second();
+                        break;
+
+                    case 3:
+                        mode = 0;
+                        Task3.Third();
+                        break;
+
+                    case 4:
+                        mode = 1;
+                        Console.WriteLine("\nДо побачення!");
+                        break;
+
+                    default:
+                        mode = 0;
+                        Console.WriteLine("\nПоздравляем, вы всё сломали!\nПопробуем снова?\n");
+                        break;
+                }
+            }
+        }
+    }
+}
+
---
+++ Task3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Laba3
+{
+    public class Task3
+    {
+        public static void Third()
+        {
+            int size = 10;
+            var listOfNums = new List<int>(size);
+
+            var random = new Random();
+            for (int i = 0; i < size; i++) listOfNums.Add(random.Next(-15, 15));
+
+            Console.WriteLine("––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+            Console.Write($"Целые числа: {string.Join(", ", listOfNums)}");
+
+            var positive = string.Join(", ", listOfNums.Where(n => n > 0));
+            Console.WriteLine($"\nПоложительные числа: {positive} ");
+            Console.WriteLine("––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+        }
+    }
+}
